23dad9838ea6c644be40e233e2f8af3d28b87946
Talisson Rodrigues <72582696+iamtomorrow@users.noreply.github.com>
2021-11-30 12:17:23 -0300

    Create main13.js

---
+++ JavaScript/AlgorithmsScripts/main13.js
@@ -0,0 +1,28 @@
+
+function myReplace(str, before, after) {
+  str = str.split(" ");
+  let upperCaseMatching = before.match(/^[A-Z]/);
+
+  str.forEach(word => {
+    if (word === before) {
+      let newIndexOfAfter = str.indexOf(before);
+      let newAfter = after.split('');
+
+      if (upperCaseMatching != null) {
+        newAfter.splice(0, 1, newAfter[0].toUpperCase());
+        newAfter = newAfter.join('');
+        str[newIndexOfAfter] = newAfter;
+      }
+      else {
+        newAfter.splice(0, 1, newAfter[0].toLowerCase());
+        newAfter = newAfter.join('');
+        str[newIndexOfAfter] = newAfter;
+      }
+    }
+  })
+
+  str = str.join(" ");
+  return str;
+}
+
+console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms"));
